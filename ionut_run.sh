e3e39d47edb093e8fbc51e85eaeec542b3e7ad35
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-06-30 14:52:11 +0200

    renamed files

--- ionut_run.sh
+++ ionut_run.sh
@@ -2,6 +2,10 @@
 
 clear
 
-# tune run full_finetune_single_device --config recipes/configs/imodoranu/llama3.1_8B_full_single_device.yaml
+# tune run full_finetune_single_device --config recipes/configs/imodoranu/llama3.1_8B_full_ft_single_gpu.yaml
 
-tune run --nproc_per_node 4 full_finetune_distributed --config recipes/configs/imodoranu/llama3.1_8B_full_distributed.yaml
+tune run \
+  --nproc_per_node 8 \
+  full_finetune_distributed \
+  --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml \
+  --batch_size
