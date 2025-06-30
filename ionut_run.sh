a322b9e5e0d4abf3c18ef28f348c9130f14d4fa9
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-06-30 21:34:57 +0200

    added for loop to iterate through optimizers

--- ionut_run.sh
+++ ionut_run.sh
@@ -4,7 +4,16 @@ clear
 
 # tune run full_finetune_single_device --config recipes/configs/imodoranu/llama3.1_8B_full_ft_single_gpu.yaml
 
-tune run \
-    --nproc_per_node 8 \
-    full_finetune_distributed \
-    --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml
+for optimizer in "adamw" "frugal_micro_adam"; do
+    tune run \
+        --nproc_per_node 8 \
+        full_finetune_distributed \
+        --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml \
+        optimizer.name=$optimizer
+done
+
+#tune run \
+#    --nproc_per_node 8 \
+#    full_finetune_distributed \
+#    --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml \
+#    optimizer.name=adamw
