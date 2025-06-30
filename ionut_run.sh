d462195e8966f6103dfb584ff5112f3a5e815fd0
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-06-30 16:01:54 +0200

    added support for FrugalMicroAdamW

--- ionut_run.sh
+++ ionut_run.sh
@@ -5,7 +5,6 @@ clear
 # tune run full_finetune_single_device --config recipes/configs/imodoranu/llama3.1_8B_full_ft_single_gpu.yaml
 
 tune run \
-  --nproc_per_node 8 \
-  full_finetune_distributed \
-  --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml \
-  --batch_size
+    --nproc_per_node 8 \
+    full_finetune_distributed \
+    --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml
