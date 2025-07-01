9ac8403c49bcea492afa87281f9339373fc81aa6
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-01 12:20:02 +0200

    moved script to a separate folder

---
+++ scripts/ionut_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+
+# tune run full_finetune_single_device --config recipes/configs/imodoranu/llama3.1_8B_full_ft_single_gpu.yaml
+
+#for optimizer in "adamw" "frugal_micro_adam"; do
+#    tune run \
+#        --nproc_per_node 8 \
+#        full_finetune_distributed \
+#        --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml \
+#        optimizer.name=$optimizer \
+#        dataset.split=train_1M
+#done
+
+tune run \
+    --nproc_per_node 8 \
+    full_finetune_distributed \
+    --config recipes/configs/imodoranu/llama3.1_8B_full_ft_multi_gpu.yaml
