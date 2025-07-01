c3d96e0cb73d0c91165a1687d26fd7a3e4fa92e5
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-01 12:39:29 +0200

    added download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+clear
 
 MODEL="meta-llama/Llama-3.2-1B-Instruct"
 OUTPUT_DIR="${HF_HOME}/Llama-3.2-1B-Instruct"
 
-echo $MODEL
-echo $OUTPUT_DIR
-
-#tune download $MODEL --output-dir $OUTPUT_DIR --ignore-patterns "original/consolidated.00.pth"
+echo "🚀 Downloading '${MODEL}' to '${OUTPUT_DIR}'"
+tune download $MODEL --output-dir $OUTPUT_DIR --ignore-patterns "original/consolidated.00.pth"
