8b5d48cf9c4c29f8a84cb0482d864cefb0643bda
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-02 11:38:55 +0200

    removed frugal_micro_adam and work only with the FSDP-2 version

--- scripts/ionut_run.sh
+++ scripts/ionut_run.sh
@@ -7,7 +7,7 @@ MODEL="3.2_1B"
 
 # tune run full_finetune_single_device --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_single_gpu.yaml"
 
-#for optimizer in "adamw" "frugal_micro_adam"; do
+#for optimizer in "adamw" "frugal_micro_adam_fsdp2"; do
 #    tune run \
 #        --nproc_per_node 8 \
 #        full_finetune_distributed \
@@ -18,7 +18,6 @@ MODEL="3.2_1B"
 
 
 OPTIMIZER=adamw
-#OPTIMIZER=frugal_micro_adam
 #OPTIMIZER=frugal_micro_adam_fsdp2
 
 LR=1e-5
