80c204dc147a239a5b63359e9631fdb03907d3f4
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-02 11:41:48 +0200

    updated bash script

--- scripts/ionut_run.sh
+++ scripts/ionut_run.sh
@@ -7,23 +7,14 @@ MODEL="3.2_1B"
 
 # tune run full_finetune_single_device --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_single_gpu.yaml"
 
-#for optimizer in "adamw" "frugal_micro_adam_fsdp2"; do
-#    tune run \
-#        --nproc_per_node 8 \
-#        full_finetune_distributed \
-#        --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml" \
-#        optimizer.name=$optimizer \
-#        dataset.split=train_1M
-#done
-
-
 OPTIMIZER=adamw
 #OPTIMIZER=frugal_micro_adam_fsdp2
 
-LR=1e-5
-tune run \
-    --nproc_per_node 8 \
-    full_finetune_distributed \
-    --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml" \
-    optimizer_name=${OPTIMIZER} \
-    learning_rate=${LR}
+for LR in 1e-5 2e-5 3e-5; do
+    tune run \
+        --nproc_per_node 8 \
+        full_finetune_distributed \
+        --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml" \
+        optimizer_name=${OPTIMIZER} \
+        learning_rate=${LR}
+done
