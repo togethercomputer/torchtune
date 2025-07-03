9d0a903aa0b847fddf4e756aacdc635f5d3a20af
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-03 16:50:22 +0200

    added use_sign_sgd

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,14 +7,16 @@ MODEL="3.2_1B"
 
 # tune run full_finetune_single_device --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_single_gpu.yaml"
 
-OPTIMIZER=adamw
-#OPTIMIZER=frugal_micro_adam_fsdp2
+#OPTIMIZER=adamw
+OPTIMIZER=frugal_micro_adam_fsdp2
 
-for LR in 1e-5 2e-5 3e-5; do
+for LR in 9e-6 8e-6 7e-6; do
     tune run \
         --nproc_per_node 8 \
         full_finetune_distributed \
         --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml" \
         optimizer_name=${OPTIMIZER} \
-        learning_rate=${LR}
+        learning_rate=${LR} \
+        optimizer.frugal_micro_adam_fsdp2.use_sign_sgd=0 \
+        optimizer.frugal_micro_adam_fsdp2.normalization=none
 done
