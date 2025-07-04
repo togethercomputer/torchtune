bb479c53ee6b84f8704bafdba80db38fd83d526f
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-04 02:34:30 +0200

    changed quant block size to 64 and updated run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,15 +8,19 @@ MODEL="3.2_1B"
 # tune run full_finetune_single_device --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_single_gpu.yaml"
 
 #OPTIMIZER=adamw
-OPTIMIZER=frugal_micro_adam_fsdp2
+#OPTIMIZER=frugal_micro_adam_fsdp2
+OPTIMIZER=micro_adam_cuda_fsdp2
 
-for LR in 9e-6 8e-6 7e-6; do
+for LR in 9e-6 8e-6 7e-6 6e-6 5e-6; do
     tune run \
         --nproc_per_node 8 \
         full_finetune_distributed \
         --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml" \
         optimizer_name=${OPTIMIZER} \
         learning_rate=${LR} \
-        optimizer.frugal_micro_adam_fsdp2.use_sign_sgd=0 \
-        optimizer.frugal_micro_adam_fsdp2.normalization=none
+        optimizer.micro_adam_cuda_fsdp2.decay_all_weights=1
 done
+
+
+#        optimizer.frugal_micro_adam_fsdp2.use_sign_sgd=0 \
+#        optimizer.frugal_micro_adam_fsdp2.normalization=none
