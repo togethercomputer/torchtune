4c028c0802734cec70ece28602af49e5267bf314
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-07 15:28:00 +0200

    added eval scripts

---
+++ evaluation/core_eval.py
@@ -0,0 +1,124 @@
+import sys
+sys.path.append('/home/imodoranu/workplace/projects/torchtune/evaluation')
+import os
+import time
+import json
+import argparse
+import wandb
+from evaluation import evaluate_model
+
+def convert_seconds_to_hours_minutes_seconds(ss):
+    ss = int(ss)
+    hh = ss // 3600
+    ss %= 3600
+    mm = ss // 60
+    ss %= 60
+    return f'{hh}h {mm}m {ss}s'
+
+def get_arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csv_args',       type=str, default=None, required=False,  help='The root folder that contains the experiment')
+    parser.add_argument('--root',           type=str, default=None, required=False, help='The root folder that contains the experiment')
+    parser.add_argument('--wandb_project',  type=str, default=None, required=False, help='The wandb project inside "ist" owner.')
+    parser.add_argument('--wandb_group',    type=str, default=None, required=False, help='The wandb group in the project.')
+    parser.add_argument('--wandb_job_type', type=str, default=None, required=False, help='The wandb job type')
+    parser.add_argument('--wandb_name',     type=str, default=None, required=False, help='The name for the experiment in wandb runs')
+    parser.add_argument('--wandb_run_id',   type=str, default=None, required=False, help='The run_id of the wandb job.')
+    parser.add_argument('--task',           type=str, default=None, required=False, help='The task to evaluate the model on')
+    parser.add_argument('--batch_size',     type=int, default=None, required=False, help='The batch size to use for evaluation.')
+    parser.add_argument('--out_folder',     type=str, default=None, required=False, help='A placeholder for GridSearcher')
+    args = parser.parse_args()
+
+    # --arg will contain all other arguments below it, but separated by a space
+    if args.csv_args is not None:
+        for item in args.csv_args.split(','):
+            split = item.split('=')
+            key = split[0]
+            value = '='.join(split[1:])
+            setattr(args, key, value)
+    return args
+
+def model_eval(args):
+    print(f'\nRunning eval on task {args.task}\n')
+
+    ft_model_path = os.path.join(
+        args.root,
+        args.wandb_project,
+        f'{args.wandb_group}_{args.wandb_job_type}_{args.wandb_name}',
+        # 'huggingface'
+        'epoch_0',
+    )
+
+    print(f'{ft_model_path}')
+
+    if not os.path.isdir(ft_model_path):
+        print('Path not found, exiting')
+        return None
+
+    print('Evaluating model...')
+
+    eval_metrics_file = os.path.join(ft_model_path, 'eval', f'{args.task}_eval_metrics.json')
+
+    print(f'\n\n{eval_metrics_file=}\n\n')
+
+    if os.path.isfile(eval_metrics_file):
+        print(f'Evaluation metrics file found: {eval_metrics_file}')
+        with open(eval_metrics_file, 'r') as file:
+            data = json.load(file)
+            if args.task == 'gsm8k':
+                eval_data = {
+                    f'eval/{args.task}/acc': data['results']['gsm8k']['acc'],
+                    f'eval/{args.task}/acc-std': data['results']['gsm8k']['acc_stderr'],
+                    f'eval/{args.task}/elapsed': 'unknown'
+                }
+            elif args.task in ['viggo', 'sql']:
+                eval_data = {
+                    f'eval/{args.task}/acc': data['acc'],
+                    f'eval/{args.task}/acc-std': data['acc_std'],
+                    f'eval/{args.task}/elapsed': 'unknown'
+                }
+            elif args.task == 'math':
+                print(f'Evaluation for MATH dataset:')
+                print(data)
+            else:
+                raise NotImplementedError(f'Reading eval data for task {args.task} is not implemented yet!')
+    else:
+        print(f'Evaluation metrics file not found: {eval_metrics_file}')
+        time_eval_start = time.time()
+        acc, acc_std = evaluate_model(
+            ft_model_path=ft_model_path,
+            task=args.task,
+            batch_size=args.batch_size)
+        time_eval_end = time.time()
+
+        eval_data = {
+            f'eval/{args.task}/acc': acc,
+            f'eval/{args.task}/acc-std': acc_std,
+            f'eval/{args.task}/elapsed': convert_seconds_to_hours_minutes_seconds(time_eval_end - time_eval_start)
+        }
+    # end if-else
+    print(f'{eval_data=}')
+    return eval_data
+
+def upload_data_to_run(eval_data, args):
+    if eval_data is not None:
+        api = wandb.Api()
+        run = api.run(f'ist/{args.wandb_project}/{args.wandb_run_id}')
+        for k, v in eval_data.items():
+            if k not in run.summary:
+                run.summary[k] = v
+        run.summary.update()
+
+        ### initial version that increases Runtime
+        # run = wandb.init(entity='ist', project=args.wandb_project, id=args.wandb_run_id, resume='allow')
+        # run.log(data)
+        # run.finish()
+
+def main():
+    args = get_arg_parse()
+    eval_data = model_eval(args)
+    upload_data_to_run(eval_data, args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ evaluation/main_scrape_eval_allocated.py
@@ -0,0 +1,141 @@
+import argparse
+import wandb
+import os
+from gridsearcher import GridSearcher, GSExe, GSKeyValSep
+
+
+def get_arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filter', type=str, required=True)
+    # parser.add_argument('--exclude', default='', type=str, required=False) # 'gpu266,gpu275,gpu276,gpu277'
+    # parser.add_argument('--cpus_per_task', default=10, type=int, required=False)
+    # parser.add_argument('--time', default='10-00:00:00', type=str, required=False)
+    # parser.add_argument('--mem', default='100G', type=str, required=False)
+    # parser.add_argument('--partition', default='gpu100', type=str, required=False)
+    # parser.add_argument('--gres', default='gpu:H100:1', type=str, required=False)
+    args = parser.parse_args()
+    return args
+
+def main():
+    """
+        This script iterates through all wandb runs in a project.
+        For each run, we extract information, such as project name, group, job_type, name and run id.
+        This information is sent to the SBATCH script that actually runs core_eval.py to evaluate the model (trained in this wandb run).
+        The model was saved at the location ROOT / wandb_project / wandb_group / wandb_job_type / wandb_name / huggingface
+
+        To use this script, run **pip3 install gridsearcher**
+    """
+    args = get_arg_parse()
+
+    gpus = [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+    ]
+
+    # llama_version = 2; llama_size = 7
+    # llama_version = 3; llama_size = 8
+
+    # task = 'gsm8k'
+    # task = 'viggo'
+    # task = 'sql'
+    # task = 'math'
+
+    BATCH_SIZE = 64 # if llama_version == 2 else 32
+    # ROOT = '/mnt/beegfs/alistgrp/imodoran/results'
+    ROOT = '/data/imodoranu/results/'
+
+    MODELS = [
+        # (2, 7),
+        # (3, 8),
+        (3.2, 1),
+    ] # (version, size)
+
+    TASKS = [
+        'math',
+        'gsm8k',
+        # 'viggo',
+        # 'sql',
+    ]
+
+    core_eval_args = []
+
+    for llama_version, llama_size in MODELS:
+        for task in TASKS:
+            # wandb_project = f'ionut_clr-adamw_llama{llama_version}-{llama_size}b_{task}'
+            # wandb_project = f'imodoranu_frugal-micro-adam_llama{llama_version}-{llama_size}b_{task}'
+            wandb_project = f'ionut_torchtune_llama{llama_version}-{llama_size}B_full_ft_multi_gpu'
+
+            api = wandb.Api()
+            runs = api.runs(f'ist/{wandb_project}')
+
+            group_filters = args.filter.split(',')
+
+            for run in runs:
+                # run_str = f'{run.group}/{run.job_type}'
+                if run.state != 'finished':
+                    # print(f'skipping run {run_str} because it is not finished')
+                    continue
+                if 'eval/acc' in run.summary:
+                    # print(f'skipping run {run_str} because model was already trained')
+                    continue
+                if not any([gf in run.group for gf in group_filters]):
+                    # print(f'skipping run {run_str} because group does not contain any string from "{group_filters}"')
+                    continue
+
+                core_eval_args.append(
+                    ','.join([
+                        f'root={ROOT}',
+                        f'wandb_project={wandb_project}',
+                        f'wandb_group={run.group}',
+                        f'wandb_job_type={run.job_type}',
+                        f'wandb_name={run.name}',
+                        f'wandb_run_id={run.id}',
+                        f'task={task}',
+                        f'batch_size={BATCH_SIZE}',
+                    ])
+                )
+            # end for run
+        # end for task
+    # end for (version, size)
+
+    gs = GridSearcher(
+        # script=f'/nfs/scistore19/alistgrp/imodoran/workplace/M-FAC_extensions/llm-foundry/scripts/train/evaluation/core_eval.py',
+        script=f'/home/imodoranu/workplace/projects/torchtune/evaluation/core_eval.py',
+        exe=GSExe.PYTHON,
+        key_value_separator=GSKeyValSep.EQUAL,
+        use_dashes=True,
+        defaults=dict())
+
+    state_finished_file = './gridsearcher_output/state.finished'
+    if os.path.isfile(state_finished_file):
+        os.remove(state_finished_file)
+
+    gs.run(
+        launch_blocking=0,
+        torchrun=0,
+        scheduling=dict(
+            distributed_training=False,
+            max_jobs_per_gpu=1,
+            gpus=gpus,
+            params_values={
+                'csv_args': core_eval_args
+            }
+        ),
+        param_name_for_exp_root_folder='out_folder',
+        exp_folder='./gridsearcher_output')
+    # for i, core_arg in enumerate(core_eval_args):
+    #     print(f'{i}) {core_arg}')
+
+if __name__ == '__main__':
+    main()
+
+"""
+    Script usage:
+    py main_scrape_eval.py --filter clr_adamw
+"""
