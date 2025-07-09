53ebd4f49fdf2766134f6ac0e0a7aa2378105248
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-09 15:24:38 +0200

    adapter name of frugal-micro-adam

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,20 +4,21 @@ clear
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:False
 
-MODEL="3.1_8B"
-#MODEL="3.2_1B"
+#MODEL="3.1_8B"
+MODEL="3.2_1B"
 
 CONFIG="recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml"
 
 # tune run full_finetune_single_device --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_single_gpu.yaml"
 
 #OPTIMIZER=adamw
-#OPTIMIZER=frugal_micro_adam_fsdp2
+#OPTIMIZER=frugal_micro_adam_torch_fsdp2
+#OPTIMIZER=frugal_micro_adam_cuda_fsdp2
 #OPTIMIZER=micro_adam_cuda_fsdp2
 #OPTIMIZER=custom_ema_adamw
 
 for OPTIMIZER in adamw; do
-    for LR in 9e-6; do #  8e-6 7e-6 6e-6 5e-6; do
+    for LR in 1e-5 2e-5 3e-5; do # 5e-5 4e-5 3e-5 2e-5 1e-5 9e-6 8e-6 7e-6
         tune run \
             --nproc_per_node 8 \
             full_finetune_distributed \
@@ -30,5 +31,5 @@ done
 
 #    for EMA_DECAY in 10 25 50 100; do
 #        optimizer.custom_ema_adamw.ema_decay=${EMA_DECAY}
-#        optimizer.frugal_micro_adam_fsdp2.use_sign_sgd=0 \
-#        optimizer.frugal_micro_adam_fsdp2.normalization=none
+#        optimizer.frugal_micro_adam_torch_fsdp2.use_sign_sgd=0 \
+#        optimizer.frugal_micro_adam_torch_fsdp2.normalization=none
