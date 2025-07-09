b1575df0df06c648c31a71ccd6d267f00e8fa42c
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-09 12:33:03 +0200

    added PYTORCH_CUDA_ALLOC_CONF=expandable_segments:False

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,12 @@
 
 clear
 
-#MODEL="3.1_8B"
-MODEL="3.2_1B"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:False
+
+MODEL="3.1_8B"
+#MODEL="3.2_1B"
+
+CONFIG="recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml"
 
 # tune run full_finetune_single_device --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_single_gpu.yaml"
 
@@ -12,19 +16,19 @@ MODEL="3.2_1B"
 #OPTIMIZER=micro_adam_cuda_fsdp2
 #OPTIMIZER=custom_ema_adamw
 
-for OPTIMIZER in custom_ema_adamw; do
-    for EMA_DECAY in 10 25 50 100; do
-        for LR in 9e-6 8e-6 7e-6; do
-            tune run \
-                --nproc_per_node 8 \
-                full_finetune_distributed \
-                --config "recipes/configs/imodoranu/llama${MODEL}_full_ft_multi_gpu.yaml" \
-                optimizer_name=${OPTIMIZER} \
-                learning_rate=${LR} \
-                optimizer.custom_ema_adamw.ema_decay=${EMA_DECAY}
-        done
+for OPTIMIZER in adamw; do
+    for LR in 9e-6; do #  8e-6 7e-6 6e-6 5e-6; do
+        tune run \
+            --nproc_per_node 8 \
+            full_finetune_distributed \
+            --config $CONFIG \
+            optimizer_name=${OPTIMIZER} \
+            learning_rate=${LR}
     done
 done
 
+
+#    for EMA_DECAY in 10 25 50 100; do
+#        optimizer.custom_ema_adamw.ema_decay=${EMA_DECAY}
 #        optimizer.frugal_micro_adam_fsdp2.use_sign_sgd=0 \
 #        optimizer.frugal_micro_adam_fsdp2.normalization=none
