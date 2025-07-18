a780ffe5da5d3edc2c01dfb3fdf8cb526dd4b550
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-19 00:01:48 +0200

    added adam_lr_scaling_factor

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,8 +4,8 @@ clear
 
 #export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-MODEL_VERSION_SIZE="3.1-8B"
-# MODEL_VERSION_SIZE="3.2-1B"
+#MODEL_VERSION_SIZE="3.1-8B"
+ MODEL_VERSION_SIZE="3.2-1B"
 # MODEL_VERSION_SIZE="3.2-3B"
 
 CONFIG="recipes/configs/imodoranu/llama3_full_ft_multi_gpu.yaml"
@@ -16,17 +16,20 @@ CONFIG="recipes/configs/imodoranu/llama3_full_ft_multi_gpu.yaml"
 #OPTIMIZER=micro_adam_cuda_fsdp2
 #OPTIMIZER=custom_ema_adamw
 #OPTIMIZER=signsgd
+#OPTIMIZER=trionosd
 
-for OPTIMIZER in frugal_micro_adam_torch_fsdp2; do
-    for LR in 2e-5; do #  2e-5 1e-5 9e-6 8e-6 7e-6 6e-6 5e-6; do # 5e-5 4e-5 3e-5 2e-5 1e-5 9e-6 8e-6 7e-6
-        tune run \
-            --nproc_per_node 8 \
+for OPTIMIZER in trionosd; do
+    for LR in 1e-3; do #  2e-5 1e-5 9e-6 8e-6 7e-6 6e-6 5e-6; do # 5e-5 4e-5 3e-5 2e-5 1e-5 9e-6 8e-6 7e-6
+        CUDA_VISIBLE_DEVICES=0 tune run \
+            --nproc_per_node 1 \
             full_finetune_distributed \
             --config $CONFIG \
             model_version_size=${MODEL_VERSION_SIZE} \
             optimizer_name=${OPTIMIZER} \
             learning_rate=${LR} \
-            weight_decay=0
+            weight_decay=0 \
+            batch_size=8 \
+            gradient_accumulation_steps=64
     done
 done
 
