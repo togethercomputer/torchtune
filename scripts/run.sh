6541badfd312a5ba94e8067d8231072442449df1
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-07-24 13:53:54 +0200

    added automatic evaluation

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ clear
 #export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 #MODEL_VERSION_SIZE="3.1-8B"
- MODEL_VERSION_SIZE="3.2-1B"
+MODEL_VERSION_SIZE="3.2-1B"
 # MODEL_VERSION_SIZE="3.2-3B"
 
 CONFIG="recipes/configs/imodoranu/llama3_full_ft_multi_gpu.yaml"
@@ -18,21 +18,30 @@ CONFIG="recipes/configs/imodoranu/llama3_full_ft_multi_gpu.yaml"
 #OPTIMIZER=signsgd
 #OPTIMIZER=trionosd
 
-for OPTIMIZER in trionosd; do
-    for LR in 1e-2; do # 2e-5 1e-5 9e-6 8e-6 7e-6 6e-6 5e-6; do # 5e-5 4e-5 3e-5 2e-5 1e-5 9e-6 8e-6 7e-6
-        CUDA_VISIBLE_DEVICES=3 tune run \
-            --nproc_per_node 1 \
+for OPTIMIZER in signsgd; do
+    for LR in 3e-5 2e-5 1e-5; do # 2e-5 1e-5 9e-6 8e-6 7e-6 6e-6 5e-6; do # 5e-5 4e-5 3e-5 2e-5 1e-5 9e-6 8e-6 7e-6
+#        CUDA_VISIBLE_DEVICES=0
+        tune run \
+            --nproc_per_node 8 \
             full_finetune_distributed \
             --config $CONFIG \
             model_version_size=${MODEL_VERSION_SIZE} \
             optimizer_name=${OPTIMIZER} \
             learning_rate=${LR} \
-            weight_decay=0 \
-            batch_size=8 \
-            gradient_accumulation_steps=64 \
-            optimizer.trionosd.ns_lr=1e-3 \
-            optimizer.trionosd.ns_iters=10
+            weight_decay=0.01 \
+            batch_size=2 \
+            gradient_accumulation_steps=32
+#            optimizer.trionosd.ns_lr=1e-3 \
+#            optimizer.trionosd.ns_iters=1 \
+#            optimizer.trionosd.ns_reg_U=200 \
+#            optimizer.trionosd.ns_reg_V=200
     done
+
+    python3 ~/workplace/projects/torchtune/evaluation/main_scrape_eval_allocated.py \
+        --filter=${OPTIMIZER} \
+        --model_version_size=${MODEL_VERSION_SIZE} \
+        --exact=0 \
+        --shots=8
 done
 
 #    for EMA_DECAY in 10 25 50 100; do
